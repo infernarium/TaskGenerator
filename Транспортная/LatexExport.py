a5f203a4aa92f22846a5a3a4aa565d0b5eab9a5d
Infernarium <mark.tumanyan@yandex.ru>
2024-05-05 18:46:41 +0300

    Базовое формирование tex документа

--- Транспортная/LatexExport.py
+++ Транспортная/LatexExport.py
@@ -1,8 +1,24 @@
-class Document:
-    ...
+from pylatex import Document, Section, Subsection, Command, LineBreak, Package, NewLine
+
+
+def GenerateLatex(save_path: str = "Транспортные задачи", hlines: bool = False):
+    doc = Document(document_options="a4paper,12pt", fontenc=None, lmodern=None, textcomp=None, page_numbers=None)
     
-def GenerateLatex():
-    ...
+    doc.packages.append(Package("fontenc", options="T2A")) #удалить если будет вызывать ошибки
+    doc.packages.append(Package("babel", options="russian"))
     
-def SaveLatex():
-    ...
+    with doc.create(Section('Транспортные задачи', label=False, numbering=False)):
+        doc.append("Цель задачи - составление оптимального плана перевозок из пунктов доставки в пункты выдачи.")
+        doc.append(LineBreak())
+        doc.append("Этапы решения: построение начального опорного плана, расчёт потенциалов, расчёт матриц, проверка оптимальности.")
+        doc.append(LineBreak())
+        
+        for i in range(5):
+            with doc.create(Subsection(f'Вариант {i}', label=False, numbering=False)):
+                doc.append("asdasdasdasd")
+                doc.append(LineBreak())
+                
+    doc.generate_tex(save_path)
+
+if __name__ == "__main__":
+    GenerateLatex(hlines=True)
